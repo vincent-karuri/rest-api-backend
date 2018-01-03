d02d89570e13ef920bb7b784f4b802bf8bf3deaf
vkaruri <vkaruri@princeton.edu>
2018-01-03 16:12:02 -0500

    refactored index.py to use the new model transaction classes

--- index.py
+++ index.py
@@ -1,21 +1,47 @@
 from flask import Flask, jsonify, request
+from model.expense import Expense, ExpenseSchema
+from model.income import Income, IncomeSchema
+from model.transaction_type import  TransactionType
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "Hello, World!"
 
-incomes = [
-    {'description': 'salary', 'amount': 5000}
+transactions = [
+   Income('Salary', 5000),
+   Income('Dividends', 200),
+   Expense('Pizza', 50),
+   Expense('Rock Concert', 100)
 ]
 
 @app.route('/incomes')
 def get_incomes():
-    return jsonify(incomes)
+    schema = IncomeSchema(many=True)
+    incomes = schema.dump(
+        filter(lambda t: t.type == TransactionType.INCOME, transactions)
+    )
+    return jsonify(incomes.data)
 
 @app.route('/incomes', methods=['POST'])
 def add_income():
-    incomes.append(request.get_json())
+    income = IncomeSchema().load(request.get_json())
+    transactions.append(income.data)
+    return '', 204
+
+@app.route('/expenses')
+def get_expenses():
+    schema = ExpenseSchema(many=True)
+    expenses = schema.dump(
+        filter(lambda t: t.type == TransactionType.EXPENSE, transactions)
+    )
+    return jsonify(expenses.data)
+
+@app.route('/expenses', methods=['POST'])
+def add_expense():
+    expense = ExpenseSchema().load(request.get_json())
+    transactions.append(expense.data)
     return '', 204
 
 if __name__ == "__main__":
